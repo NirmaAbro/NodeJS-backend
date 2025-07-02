66d22a7ca2996450a30016472e7dfe103e9e07c8
Nirma Abro <NirmaAbro143@gmail.com>
2025-07-02 17:07:04 -0600

    building http server

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("hellow world");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.write("Hello World!");
+    res.end();
+  } else if (req.url === "/about") {
+    res.write("this is about page");
+    res.end();
+  }
+});
+
+const Port = 3000;
+server.listen(Port, () => {
+  console.log(`Server running at http://localhost:${Port}/`);
+});
