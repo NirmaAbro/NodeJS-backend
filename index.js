4a2f7ef730d286ea35574ba37dbc519f49996d53
Nirma Abro <NirmaAbro143@gmail.com>
2025-07-02 22:57:25 -0600

    get started with express js , handling routes https request

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 const http = require("http");
+const express = require("express");
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.write("Hello World!");
-    res.end();
-  } else if (req.url === "/about") {
-    res.write("this is about page");
-    res.end();
-  }
+const app = express();
+
+// app is basically a handler function
+
+app.get("/", (req, res) => {
+  res.send("hey from home page");
 });
 
+app.get("/about", (req, res) => {
+    return res.send(`hello ${req.query.name} your age is`);
+  });
+  
+  
+
+// in nodejs
+// const server = http.createServer((req, res) => {
+//   if (req.url === "/") {
+//     res.write("Hello World!");
+//     res.end();
+//   } else if (req.url === "/about") {
+//     res.write("this is about page");
+//     res.end();
+//   }
+// });
+
 const Port = 3000;
-server.listen(Port, () => {
+app.listen(Port, () => {
   console.log(`Server running at http://localhost:${Port}/`);
 });
